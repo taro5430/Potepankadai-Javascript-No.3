370edef4160c6ba8b8b847a32ab6c6c9a9cc569b
EC2 Default User <ec2-user@ip-172-31-87-140.ec2.internal>
2021-05-19 06:19:29 +0000

    javascriptの課題３です

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function Search(text){
+  $(".keyword_list").each(function(index,element){
+    let keyword = $(element).text();
+    if(keyword.indexOf(text) === -1){
+      $(element).parent().css("display","none");
+    }else{
+      $(element).parent().css("display","block");
+    }
+  });
+}
+
+$(".input_text").on("input",function(event){
+  let text = $(".input_text").val();
+  if(text === ""){
+    $(".search-item").css("display","block");
+  }else{
+    Search(text);
+  }
+});
